6a22ba49665d49caa7b288759e2c47368e6d0f91
Nifyr <nintendofyr@gmail.com>
2025-09-02 11:03:23 +0200

    Define entity Task

---
+++ Entities/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoCrud.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; } // Primary key
+        public string Title { get; set; } = string.Empty; // min length 1, max length 140
+        public bool Completed { get; set; }
+        public DateTime? DueDate { get; set; }
+        public IEnumerable<string> Tags { get; set; } = Array.Empty<string>();
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
