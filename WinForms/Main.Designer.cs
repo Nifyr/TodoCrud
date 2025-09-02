56c0d31f901ac0430583225eb1b7407351efb806
Nifyr <nintendofyr@gmail.com>
2025-09-02 17:25:53 +0200

    Create Task GUI

--- WinForms/Main.Designer.cs
+++ WinForms/Main.Designer.cs
@@ -36,11 +36,36 @@
             button2 = new Button();
             button3 = new Button();
             button4 = new Button();
-            listBox1 = new ListBox();
+            taskListBox = new ListBox();
             panel2 = new Panel();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            idTextBox = new TextBox();
+            titleTextBox = new TextBox();
+            completedCheckBox = new CheckBox();
+            createdAtDateTimePicker = new DateTimePicker();
+            updatedAtDateTimePicker3 = new DateTimePicker();
+            dueDateDateTimePicker = new DateTimePicker();
+            dueDateCheckBox = new CheckBox();
+            tableLayoutPanel4 = new TableLayoutPanel();
+            tagsListBox = new ListBox();
+            tableLayoutPanel5 = new TableLayoutPanel();
+            tagTextBox = new TextBox();
+            removeTagButton = new Button();
+            addTagButton = new Button();
             tableLayoutPanel1.SuspendLayout();
             panel1.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
+            panel2.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
+            tableLayoutPanel4.SuspendLayout();
+            tableLayoutPanel5.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -49,7 +74,7 @@
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 80F));
             tableLayoutPanel1.Controls.Add(panel1, 0, 0);
-            tableLayoutPanel1.Controls.Add(listBox1, 0, 1);
+            tableLayoutPanel1.Controls.Add(taskListBox, 0, 1);
             tableLayoutPanel1.Controls.Add(panel2, 1, 1);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
@@ -147,23 +172,287 @@
             button4.Text = "Delete";
             button4.UseVisualStyleBackColor = true;
             // 
-            // listBox1
+            // taskListBox
             // 
-            listBox1.Dock = DockStyle.Fill;
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(3, 40);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(154, 407);
-            listBox1.TabIndex = 1;
+            taskListBox.Dock = DockStyle.Fill;
+            taskListBox.FormattingEnabled = true;
+            taskListBox.ItemHeight = 15;
+            taskListBox.Location = new Point(3, 40);
+            taskListBox.Name = "taskListBox";
+            taskListBox.Size = new Size(154, 407);
+            taskListBox.TabIndex = 1;
+            taskListBox.SelectedIndexChanged += TaskListBox_SelectedIndexChanged;
             // 
             // panel2
             // 
+            panel2.Controls.Add(tableLayoutPanel3);
             panel2.Dock = DockStyle.Fill;
             panel2.Location = new Point(163, 40);
             panel2.Name = "panel2";
             panel2.Size = new Size(634, 407);
             panel2.TabIndex = 2;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 3;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel3.Controls.Add(label1, 0, 0);
+            tableLayoutPanel3.Controls.Add(label2, 0, 1);
+            tableLayoutPanel3.Controls.Add(label3, 0, 2);
+            tableLayoutPanel3.Controls.Add(label4, 0, 3);
+            tableLayoutPanel3.Controls.Add(label5, 0, 4);
+            tableLayoutPanel3.Controls.Add(label6, 0, 5);
+            tableLayoutPanel3.Controls.Add(label7, 0, 6);
+            tableLayoutPanel3.Controls.Add(idTextBox, 1, 0);
+            tableLayoutPanel3.Controls.Add(titleTextBox, 1, 1);
+            tableLayoutPanel3.Controls.Add(completedCheckBox, 1, 2);
+            tableLayoutPanel3.Controls.Add(createdAtDateTimePicker, 1, 4);
+            tableLayoutPanel3.Controls.Add(updatedAtDateTimePicker3, 1, 5);
+            tableLayoutPanel3.Controls.Add(dueDateDateTimePicker, 2, 3);
+            tableLayoutPanel3.Controls.Add(dueDateCheckBox, 1, 3);
+            tableLayoutPanel3.Controls.Add(tableLayoutPanel4, 1, 6);
+            tableLayoutPanel3.Dock = DockStyle.Fill;
+            tableLayoutPanel3.Location = new Point(0, 0);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 7;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle());
+            tableLayoutPanel3.RowStyles.Add(new RowStyle());
+            tableLayoutPanel3.RowStyles.Add(new RowStyle());
+            tableLayoutPanel3.RowStyles.Add(new RowStyle());
+            tableLayoutPanel3.RowStyles.Add(new RowStyle());
+            tableLayoutPanel3.RowStyles.Add(new RowStyle());
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel3.Size = new Size(634, 407);
+            tableLayoutPanel3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Location = new Point(33, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(17, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Id";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(30, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Title";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.None;
+            label3.AutoSize = true;
+            label3.Location = new Point(9, 60);
+            label3.Name = "label3";
+            label3.Size = new Size(66, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Completed";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.None;
+            label4.AutoSize = true;
+            label4.Location = new Point(15, 85);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Due date";
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.None;
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 114);
+            label5.Name = "label5";
+            label5.Size = new Size(78, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Creation date";
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.None;
+            label6.AutoSize = true;
+            label6.Location = new Point(8, 143);
+            label6.Name = "label6";
+            label6.Size = new Size(68, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Last update";
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.None;
+            label7.AutoSize = true;
+            label7.Location = new Point(26, 278);
+            label7.Name = "label7";
+            label7.Size = new Size(31, 15);
+            label7.TabIndex = 6;
+            label7.Text = "Tags";
+            // 
+            // idTextBox
+            // 
+            tableLayoutPanel3.SetColumnSpan(idTextBox, 2);
+            idTextBox.Dock = DockStyle.Fill;
+            idTextBox.Enabled = false;
+            idTextBox.Location = new Point(87, 3);
+            idTextBox.Name = "idTextBox";
+            idTextBox.Size = new Size(544, 23);
+            idTextBox.TabIndex = 7;
+            // 
+            // titleTextBox
+            // 
+            tableLayoutPanel3.SetColumnSpan(titleTextBox, 2);
+            titleTextBox.Dock = DockStyle.Fill;
+            titleTextBox.Enabled = false;
+            titleTextBox.Location = new Point(87, 32);
+            titleTextBox.Name = "titleTextBox";
+            titleTextBox.Size = new Size(544, 23);
+            titleTextBox.TabIndex = 8;
+            // 
+            // completedCheckBox
+            // 
+            completedCheckBox.Anchor = AnchorStyles.Left;
+            completedCheckBox.AutoSize = true;
+            tableLayoutPanel3.SetColumnSpan(completedCheckBox, 2);
+            completedCheckBox.Enabled = false;
+            completedCheckBox.Location = new Point(87, 61);
+            completedCheckBox.Name = "completedCheckBox";
+            completedCheckBox.Size = new Size(15, 14);
+            completedCheckBox.TabIndex = 9;
+            completedCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // createdAtDateTimePicker
+            // 
+            tableLayoutPanel3.SetColumnSpan(createdAtDateTimePicker, 2);
+            createdAtDateTimePicker.Dock = DockStyle.Fill;
+            createdAtDateTimePicker.Enabled = false;
+            createdAtDateTimePicker.Location = new Point(87, 110);
+            createdAtDateTimePicker.Name = "createdAtDateTimePicker";
+            createdAtDateTimePicker.Size = new Size(544, 23);
+            createdAtDateTimePicker.TabIndex = 11;
+            // 
+            // updatedAtDateTimePicker3
+            // 
+            tableLayoutPanel3.SetColumnSpan(updatedAtDateTimePicker3, 2);
+            updatedAtDateTimePicker3.Dock = DockStyle.Fill;
+            updatedAtDateTimePicker3.Enabled = false;
+            updatedAtDateTimePicker3.Location = new Point(87, 139);
+            updatedAtDateTimePicker3.Name = "updatedAtDateTimePicker3";
+            updatedAtDateTimePicker3.Size = new Size(544, 23);
+            updatedAtDateTimePicker3.TabIndex = 12;
+            // 
+            // dueDateDateTimePicker
+            // 
+            dueDateDateTimePicker.Dock = DockStyle.Fill;
+            dueDateDateTimePicker.Enabled = false;
+            dueDateDateTimePicker.Location = new Point(107, 81);
+            dueDateDateTimePicker.Name = "dueDateDateTimePicker";
+            dueDateDateTimePicker.Size = new Size(524, 23);
+            dueDateDateTimePicker.TabIndex = 10;
+            // 
+            // dueDateCheckBox
+            // 
+            dueDateCheckBox.Anchor = AnchorStyles.Left;
+            dueDateCheckBox.AutoSize = true;
+            dueDateCheckBox.Enabled = false;
+            dueDateCheckBox.Location = new Point(87, 83);
+            dueDateCheckBox.Name = "dueDateCheckBox";
+            dueDateCheckBox.Size = new Size(14, 19);
+            dueDateCheckBox.TabIndex = 13;
+            dueDateCheckBox.Text = "checkBox2";
+            dueDateCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel4
+            // 
+            tableLayoutPanel4.ColumnCount = 2;
+            tableLayoutPanel3.SetColumnSpan(tableLayoutPanel4, 2);
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel4.Controls.Add(tagsListBox, 0, 0);
+            tableLayoutPanel4.Controls.Add(tableLayoutPanel5, 1, 0);
+            tableLayoutPanel4.Dock = DockStyle.Fill;
+            tableLayoutPanel4.Location = new Point(87, 168);
+            tableLayoutPanel4.Name = "tableLayoutPanel4";
+            tableLayoutPanel4.RowCount = 1;
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel4.Size = new Size(544, 236);
+            tableLayoutPanel4.TabIndex = 14;
+            // 
+            // tagsListBox
+            // 
+            tagsListBox.Dock = DockStyle.Fill;
+            tagsListBox.FormattingEnabled = true;
+            tagsListBox.ItemHeight = 15;
+            tagsListBox.Location = new Point(3, 3);
+            tagsListBox.Name = "tagsListBox";
+            tagsListBox.Size = new Size(266, 230);
+            tagsListBox.TabIndex = 0;
+            // 
+            // tableLayoutPanel5
+            // 
+            tableLayoutPanel5.ColumnCount = 2;
+            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel5.Controls.Add(tagTextBox, 0, 0);
+            tableLayoutPanel5.Controls.Add(removeTagButton, 0, 2);
+            tableLayoutPanel5.Controls.Add(addTagButton, 0, 1);
+            tableLayoutPanel5.Dock = DockStyle.Fill;
+            tableLayoutPanel5.Location = new Point(275, 3);
+            tableLayoutPanel5.Name = "tableLayoutPanel5";
+            tableLayoutPanel5.RowCount = 4;
+            tableLayoutPanel5.RowStyles.Add(new RowStyle());
+            tableLayoutPanel5.RowStyles.Add(new RowStyle());
+            tableLayoutPanel5.RowStyles.Add(new RowStyle());
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel5.Size = new Size(266, 230);
+            tableLayoutPanel5.TabIndex = 1;
+            // 
+            // tagTextBox
+            // 
+            tableLayoutPanel5.SetColumnSpan(tagTextBox, 2);
+            tagTextBox.Dock = DockStyle.Fill;
+            tagTextBox.Enabled = false;
+            tagTextBox.Location = new Point(3, 3);
+            tagTextBox.Name = "tagTextBox";
+            tagTextBox.PlaceholderText = "New tag...";
+            tagTextBox.Size = new Size(260, 23);
+            tagTextBox.TabIndex = 0;
+            // 
+            // removeTagButton
+            // 
+            removeTagButton.AutoSize = true;
+            removeTagButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            removeTagButton.Enabled = false;
+            removeTagButton.Location = new Point(3, 63);
+            removeTagButton.Name = "removeTagButton";
+            removeTagButton.Size = new Size(106, 25);
+            removeTagButton.TabIndex = 1;
+            removeTagButton.Text = "Remove selected";
+            removeTagButton.UseVisualStyleBackColor = true;
+            // 
+            // addTagButton
+            // 
+            addTagButton.AutoSize = true;
+            addTagButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            addTagButton.Enabled = false;
+            addTagButton.Location = new Point(3, 32);
+            addTagButton.Name = "addTagButton";
+            addTagButton.Size = new Size(64, 25);
+            addTagButton.TabIndex = 2;
+            addTagButton.Text = "Add new";
+            addTagButton.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
@@ -179,6 +468,12 @@
             panel1.PerformLayout();
             tableLayoutPanel2.ResumeLayout(false);
             tableLayoutPanel2.PerformLayout();
+            panel2.ResumeLayout(false);
+            tableLayoutPanel3.ResumeLayout(false);
+            tableLayoutPanel3.PerformLayout();
+            tableLayoutPanel4.ResumeLayout(false);
+            tableLayoutPanel5.ResumeLayout(false);
+            tableLayoutPanel5.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -186,7 +481,7 @@
 
         private TableLayoutPanel tableLayoutPanel1;
         private Panel panel1;
-        private ListBox listBox1;
+        private ListBox taskListBox;
         private Panel panel2;
         private TextBox textBox1;
         private Button button1;
@@ -194,5 +489,26 @@
         private Button button2;
         private Button button3;
         private Button button4;
+        private TableLayoutPanel tableLayoutPanel3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox idTextBox;
+        private TextBox titleTextBox;
+        private CheckBox completedCheckBox;
+        private DateTimePicker dueDateDateTimePicker;
+        private DateTimePicker createdAtDateTimePicker;
+        private DateTimePicker updatedAtDateTimePicker3;
+        private CheckBox dueDateCheckBox;
+        private TableLayoutPanel tableLayoutPanel4;
+        private ListBox tagsListBox;
+        private TableLayoutPanel tableLayoutPanel5;
+        private TextBox tagTextBox;
+        private Button removeTagButton;
+        private Button addTagButton;
     }
 }
--- WinForms/Main.cs
+++ WinForms/Main.cs
@@ -1,4 +1,6 @@
 using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 using TodoCrud.Entities;
 
 namespace TodoCrud.WinForms
@@ -12,13 +14,31 @@ namespace TodoCrud.WinForms
             Populate(null);
         }
 
-        private void Populate(string? searchQuery)
+        struct SearchFilter
+        {
+            public string SearchString { get; set; }
+            public bool IncludeCompleted { get; set; }
+        }
+
+        private void Populate(SearchFilter? filter)
         {
             using HttpClient client = new();
             client.BaseAddress = new Uri("http://localhost:5185/");
-            HttpResponseMessage response = client.GetAsync("tasks").Result;
+            HttpResponseMessage response;
+            try
+            {
+                response = client.GetAsync("tasks").GetAwaiter().GetResult();
+            }
+            catch (HttpRequestException)
+            {
+                // Could not reach the API
+                RetrieveFailureMessage();
+                return;
+            }
+
             if (!response.IsSuccessStatusCode)
             {
+                // Received response but not successful
                 RetrieveFailureMessage();
                 return;
             }
@@ -26,16 +46,99 @@ namespace TodoCrud.WinForms
             List<Entities.Task>? tasks = response.Content.ReadFromJsonAsync<List<Entities.Task>>().Result;
             if (tasks is null)
             {
+                // Response content could not be deserialized
                 RetrieveFailureMessage();
                 return;
             }
-
-            listBox1.DataSource = tasks;
+            taskListBox.DataSource = tasks;
         }
 
         private static void RetrieveFailureMessage()
         {
             MessageBox.Show("Failed to retrieve tasks from the API.");
+        }
+
+        private void TaskListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateTaskDisplay();
+        }
+
+        private void UpdateTaskDisplay()
+        {
+            int selectedIndex = taskListBox.SelectedIndex;
+            DisableTaskInputControls();
+            if (selectedIndex < 0)
+            {
+                // No item is selected
+                ClearTaskDisplay();
+                return;
+            }
+            Entities.Task task = (Entities.Task)taskListBox.Items[selectedIndex];
+
+            idTextBox.Text = task.Id.ToString();
+
+            titleTextBox.Text = task.Title;
+            titleTextBox.Enabled = true;
+
+            completedCheckBox.Checked = task.Completed;
+            completedCheckBox.Enabled = true;
+
+            dueDateCheckBox.Checked = task.DueDate != null;
+            dueDateCheckBox.Enabled = true;
+            if (task.DueDate is null)
+            {
+                // No due date set
+                dueDateDateTimePicker.Value = DateTime.Now;
+                dueDateDateTimePicker.Visible = false;
+                dueDateDateTimePicker.Enabled = false;
+            }
+            else
+            {
+                dueDateDateTimePicker.Value = task.DueDate.Value;
+                dueDateDateTimePicker.Visible = true;
+                dueDateDateTimePicker.Enabled = true;
+            }
+
+            createdAtDateTimePicker.Value = task.CreatedAt;
+
+            updatedAtDateTimePicker3.Value = task.UpdatedAt;
+
+            tagsListBox.DataSource = task.Tags.ToList();
+            tagsListBox.Enabled = true;
+
+            tagTextBox.Text = string.Empty;
+            tagTextBox.Enabled = true;
+
+            removeTagButton.Enabled = true;
+
+            addTagButton.Enabled = true;
+        }
+
+        private void DisableTaskInputControls()
+        {
+            idTextBox.Enabled = false;
+            titleTextBox.Enabled = false;
+            completedCheckBox.Enabled = false;
+            dueDateCheckBox.Enabled = false;
+            dueDateDateTimePicker.Enabled = false;
+            createdAtDateTimePicker.Enabled = false;
+            updatedAtDateTimePicker3.Enabled = false;
+            tagsListBox.Enabled = false;
+            removeTagButton.Enabled = false;
+            addTagButton.Enabled = false;
+        }
+
+        private void ClearTaskDisplay()
+        {
+            idTextBox.Text = string.Empty;
+            titleTextBox.Text = string.Empty;
+            completedCheckBox.Checked = false;
+            dueDateCheckBox.Checked = false;
+            dueDateDateTimePicker.Value = DateTime.Now;
+            createdAtDateTimePicker.Value = DateTime.Now;
+            updatedAtDateTimePicker3.Value = DateTime.Now;
+            tagsListBox.DataSource = null;
+            tagTextBox.Text = string.Empty;
         }
     }
 }
